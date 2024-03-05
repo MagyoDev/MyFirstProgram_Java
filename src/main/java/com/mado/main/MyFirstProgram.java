21fd8864cb426de529ad07f7340d8b5ae71b12fc
MagyoDev <alandelimasilva51@gmail.com>
2024-03-05 13:35:44 -0300

    MyFirstProgram

---
+++ src/main/java/com/mado/main/MyFirstProgram.java
@@ -0,0 +1,27 @@
+package com.mado.main;
+
+public class MyFirstProgram {
+
+    public static void main(String[] args) {
+        System.out.println("Banana");
+        System.out.println("Manga");
+        
+        System.out.println("Laranja\n");
+        System.out.println("BlueBerry");
+        
+        System.out.println("\tAbacaxi");
+        System.out.println("Melancia\n");
+        
+        System.out.println("\"Acerola\"");
+        System.out.println("\\Jaca");
+        
+        // This is a COMMENT
+        
+        /*
+        This
+        is
+        a
+        COMMENT
+        */
+    }
+}
